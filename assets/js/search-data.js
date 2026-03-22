000e4c9648a06f835ebe05c51ba84c70d6c56678
benmagnifico <72290713+benmagnifico@users.noreply.github.com>
2026-03-22 15:57:41 +0000

    Deploying to gh-pages from @ benmagnifico/benmagnifico.github.io@f7cd021cdab3afef6c988299cd4ce6e42dff1384 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -470,6 +470,13 @@ ninja.data = [{
           window.open("https://github.com/benmagnifico", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://xhslink.com/m/1hhHikGESyt", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
